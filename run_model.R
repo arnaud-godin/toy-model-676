728bfc5cbf0dca45fb502c4f51b274b4b8c79598
Arnaud Godin <arnaud.godin@mail.mcgill.ca>
2018-03-25 12:57:11 -0400

    Graphical representation for calibration

--- run_model.R
+++ run_model.R
@@ -16,42 +16,42 @@ source('SIRS_model.R')
 
 # Preparing the different data points
 year <- c(2003:2013)
-surv.prevalence <- c(.643, .699, .707, .703, .735, .750, .759, .753, .707, 
-	.696, .720)
+surv.prev <- c(.643, .699, .707, .703, .735, .750, .759, .753, .707, .696, .720)
 surv.N <- c(389, 581, 461, 566, 532, 492, 464, 489, 478, 461, 486)
-surv.I <- round(surv.prevalence * surv.N)
+surv.I <- round(surv.prev * surv.N)
 surv.S <- surv.N - surv.I
 
 # The data frame
-surv.track <- data.frame(year = year, N = surv.N, I = surv.I, S = surv.S,
- prevalence = surv.prevalence)
+surv.track <- data.frame(year = year, surv.N = surv.N, surv.I = surv.I,
+ 												 surv.S = surv.S, surv.prev = surv.prev)
 
 #The confidence interval for prevalence (binomial)
-surv.track$prev.LCI <- sapply(surv.track, function(x,y) binom.test(x = surv.track$I, y = surv.tra))
-	surv.track$prev.UCI[i] <- binom.test(surv.track$I[i],
-	 surv.track$N[i])$confint[2]
-}
+LCI <- function (x,n) round(binom.test(x,n)$conf.int[1], digits = 3)
+UCI <- function (x,n) round(binom.test(x,n)$conf.int[2], digits = 3)
+
+surv.track$prev.LCI <- mapply(LCI, surv.track$surv.I, surv.track$surv.N)
+surv.track$prev.UCI <- mapply(UCI, surv.track$surv.I, surv.track$surv.N)
 
 # ---- Run of the model ----
 
 RunModel <- function(beta=2, alpha.A=0.15, alpha.Tx=.9, gamma=0.75/100,
- sigma = 0.024, dt=.01, data=surv.track, plot = ''){
+ 										 sigma = 0.024, dt=.01, data=surv.track, plot = '') {
 	# --- Initialize the model parameters ---
 	# There are only acute infections at first
 
-	N <- 10000	# Total number of people
-	A0 <- (N*0.643)/2	# Acutely infected (Prevalent cases/2)
-	C0 <- (N*0.643)/2	# Chronic cases (Prevalent cases/2)
+	N0 <- 10000	# Total number of people
+	A0 <- N0 * 0.643 # Acutely infected (Prevalent cases/2)
+	C0 <- 0	# Chronic cases (Prevalent cases/2)
 	Tx0 <- 0
 	F0 <- 0	
-	S0 <- N-A0-C0 # The susceptibles are total population - acute
+	S0 <- N0 - A0 - C0 # The susceptibles are total population - acute
 
 	#	Sigma definition in the model
 
 	# The initial population and parameters
-	init.pop <- c('S' = S0, 'A' = A0, 'C' = C0, 'Tx' = Tx0, 'F' = F0)
+	init.pop <- c(S = S0, A = A0, C = C0, Tx = Tx0, F = F0)
 	params <- c(beta = beta, alpha.A = alpha.A,
-	 alpha.Tx = alpha.Tx, gamma = gamma, sigma = sigma)
+	 						alpha.Tx = alpha.Tx, gamma = gamma, sigma = sigma)
 
 	# Time definition for the model
 	time.0 <- 2003
@@ -63,41 +63,48 @@ RunModel <- function(beta=2, alpha.A=0.15, alpha.Tx=.9, gamma=0.75/100,
 	out <- as.data.frame(ode(init.pop, vec.time, SIRSModel, params))
 
 	# Set time to a reasonable scale
-	out$time <- time.0 + vec.time
+	out$year <- time.0 + vec.time
 	out$I <- out$A + out$C + out$F
 	out$N <- out$S + out$A + out$C + out$Tx + out$F
 	out$prev <- out$I / out$N
 
 	# Estimated prevalence in the years of the surveys 
-	out.prev <- subset(out$prev, out$time %in% c(2003:2013))
-	prev.estim <- data.frame(c(2003:2013), out.prev)
+	out.prev <- subset(out$prev, out$year %in% c(2003:2013))
+	prev.estim <- data.frame(year = c(2003:2013), model.prev = out.prev)
 
-	#Returning graphical representation
-	base.graph <- ggplot(data = out, aes(x = time)) +
-	theme(panel.grid.major.x = element_blank(),
-        title = element_text(family = 'Times', size = 12),
-        axis.title.x = element_text(family = 'Times', size = 10),
-        axis.title.y = element_text(family = 'Times', size = 10),
-        legend.title = element_text(family = 'Times', size = 10)) +
-	theme_bw()
+	# --- Returning the different values ---
 
+	# Returning graphical representation
+	base.graph <- ggplot(data = out, aes(x = year)) +
+		theme(panel.grid.major.x = element_blank(),
+					title = element_text(family = 'Times', size = 12),
+					axis.title.x = element_text(family = 'Times', size = 10),
+					axis.title.y = element_text(family = 'Times', size = 10),
+					legend.title = element_text(family = 'Times', size = 10)) +
+		theme_bw()
+
+	# For the trajectory
 	epid.trajectory <- base.graph +
-	geom_line(aes(y = S, color = 'Susceptibles')) +
-	geom_line(aes(y = I, color = 'Infectious')) +
-	geom_line(aes(y = Tx, color = 'On treatment')) +
-	labs(x = 'Time (years)', y = 'Number of people')
+		geom_line(aes(y = S, color = 'Susceptibles')) +
+		geom_line(aes(y = I, color = 'Infectious')) +
+		geom_line(aes(y = Tx, color = 'On treatment')) +
+		labs(x = 'Time (years)', y = 'Number of people')
 
 	epid.prevalence <- base.graph +
-	geom_line(aes(y = prev, color = 'Prevalence')) +
-	labs(x = 'Time (years)', y = 'Prevalence')
+		geom_line(aes(y = prev, color = 'Model Prevalence')) +
+		geom_point(data = surv.track,
+							 aes(y = surv.prev,
+									 color = 'Survey prevalence')) +
+		geom_errorbar(data = surv.track, aes(ymin = prev.LCI, ymax = prev.UCI)) +
+		labs(x = 'Time (years)', y = 'Prevalence')
 
 	#Plotting either the trajectory or the prevalence
-	if(plot == ''){
+	if (plot == '') {
 		return(prev.estim)
-	} else if (plot == 'trajectory'){
-		return(list(prev.estim = prev.estim, plot.traject = epid.trajectory))
-	} else if (plot == 'prevalence'){
-		return(list(prev.estim = prev.estim, plot.prev = epid.prevalence))
+	} else if (plot == 'trajectory') {
+		return(list(prev.estim = prev.estim, model.trajectory = epid.trajectory))
+	} else if (plot == 'prevalence') {
+		return(list(prev.estime = prev.estim, model.prevalence = epid.prevalence))
 	} else {
 		return(print('Options are: "", "trajectory", "prevalence"'))
 	}
