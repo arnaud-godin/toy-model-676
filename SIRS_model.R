51a554f490a7b02f6a3fadc8c2f28ff8a8cd4953
Arnaud Godin <arnaud.godin@mail.mcgill.ca>
2018-04-03 19:33:35 -0400

    Mixing by risk groups

--- SIRS_model.R
+++ SIRS_model.R
@@ -10,19 +10,44 @@ source('force_of_infection.R', chdir = TRUE)
 
 # ---- Model of HCV transmission ----
 
-SIRSModel <- function(t, pop, parms){
-	# The compartments
-	S <- pop[1]		# Susceptible
-	A <- pop[2]		# Acute 
-	C <- pop[3]		# Chronic
-	Tx <- pop[4]	# On treatment
-	F <- pop[5]		# Failed treatment
-	N <- S + A + C + Tx + F 	#Population size
+SISModel <- function (t, pop, parms) {
+	# --- The compartments ---
+
+	# --Low risk individuals--
+
+	SLow <- pop[1]		# Susceptible
+	ALow <- pop[2]		# Acute 
+	CLow <- pop[3]		# Chronic
+	TxLow <- pop[4]		# On treatment
+	FLow <- pop[5]		# Failed treatment
+
+	# Population size for low risk individuals
+	NLow <- SLow + ALow + CLow + TxLow + FLow 
 
 	# The infectious, which imply the assumtion that people undergoing
 	# treatment are not infectious because their viral load is low 
-	I <- A + C + F 
+	ILow <- ALow + CLow + FLow
+
+	# --High risk individuals--
+
+	SHigh <- pop[6]		#Same disposition as before	
+	AHigh <- pop[7]		
+	CHigh <- pop[8]		
+	TxHigh <- pop[9]
+	FHigh <- pop[10]
+
+	# Population size for high risk individuals
+	NHigh <- SHigh + AHigh + CHigh + TxHigh + FHigh
+
+	# Infectious in the high risk population
+	IHigh <- AHigh + CHigh + FHigh
 	
+	# --- Sigma parameter of treatment coverage ---
+
+	# The data is the following (2006
+	txRate <- data.frame(year = c(3, 6, 12, 18),
+                      r = c(0.024, 0.047, 0.05, 0.055))
+	estim.sigma <- splinefun(txRate$year, txRate$r, method = 'hyman')
 
 	############## The ODE system with parameter list #####################
 	### beta = Probability of effective contact
@@ -32,14 +57,29 @@ SIRSModel <- function(t, pop, parms){
 	### There is a total of 8 parameters to estimate
 	#######################################################################
 
+	# --- The overall differential equations model ---
+
 	with(as.list(parms), {
-		dS <- - FoI(beta, I, N) * S + alpha.A * 2 * A + (alpha.Tx) * 4 * Tx
-		dA <- FoI(beta, I, N) * S - (1 - alpha.A) * 2 * A - alpha.A * 2 * A
-		dC <- (1 - alpha.A) * 2 * A - sigma * C - gamma * C	 
-		dTx <- sigma * C - (1 - alpha.Tx) * 4 * Tx + sigma * F -
-		 alpha.Tx * 4 * Tx  
-		dF <- (1 - alpha.Tx) * 4 * Tx - sigma * F - gamma * F  
-	#Returning the results
+		# The sigma parameter
+		sigma <- estim.sigma(t)
+
+		# Computing the force of infection for both groups
+
+		FOI <- FoI(beta, ILow, NLow, IHigh, NHigh)
+
+		# The differential equation system for the low risk
+		dS <- - FOI$low * SLow + alpha.A * 2 * ALow + (alpha.Tx) * 4 * TxLow -
+      r * SLow + r * NLow
+		dA <- FOI$low * SLow - (1 - alpha.A) * 2 * ALow - alpha.A * 2 * ALow -
+		 r * ALow
+		dC <- (1 - alpha.A) * 2 * ALow - sigma * CLow - gamma * CLow	- r * CLow 
+		dTx <- sigma * CLow - (1 - alpha.Tx) * 4 * TxLow + sigma * FLow -
+		 alpha.Tx * 4 * TxLow  
+		dF <- (1 - alpha.Tx) * 4 * TxLow - sigma * FLow - gamma * FLow - r * FLow
+
+		# The differential equation system for the high risk
+
+		#Returning the results
 		result <- c(dS, dA, dC, dTx, dF)
 		list(result)
 	})
